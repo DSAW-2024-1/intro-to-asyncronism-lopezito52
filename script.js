40c4c707abd89035b4ea8ef6907397f4f724c3f3
lopezito52 <samuellohu@unisabana.edu.co>
2024-03-15 18:09:52 -0500

    1.2.0

--- script.js
+++ script.js
@@ -36,7 +36,14 @@ document.addEventListener("DOMContentLoaded", () => {
       "object-center",
       "flex",
       "flex-col",
-      "hover:shadow-lg"
+      "justify-center",
+      "items-center",
+      "text-center",
+      "hover:shadow-lg",
+      "hover:translate-y-1",
+      "transition",
+      "duration-300",
+      "ease-in-out"
     );
 
     const imgElement = document.createElement("img");
@@ -49,9 +56,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const quoteTextElement = document.createElement("p");
     quoteTextElement.textContent = quote;
+    quoteTextElement.classList.add("self-end");
 
     const characterElement = document.createElement("p");
     characterElement.textContent = character;
+    characterElement.classList.add("self-end");
 
     textElement.appendChild(characterElement);
     textElement.appendChild(quoteTextElement);
@@ -70,30 +79,80 @@ document.addEventListener("DOMContentLoaded", () => {
     loadQuotes();
   });
 
-  // Función para buscar citas por personaje
-  function searchQuotes() {
-    const searchTerm = searchInput.value.trim().toLowerCase();
-    const articles = Array.from(quoteContainer.children);
-
-    articles.forEach((article) => {
-      const characterElement = article.querySelector("p:first-child");
-      const characterName = characterElement.textContent.trim().toLowerCase();
-
-      if (characterName.includes(searchTerm)) {
-        article.style.display = "block";
-      } else {
-        article.style.display = "none";
-      }
-    });
-  }
-
   // Agregar evento al botón de búsqueda
   searchButton.addEventListener("click", () => {
     searchQuotes();
   });
 
-  // Agregar evento al campo de búsqueda
-  searchInput.addEventListener("input", () => {
-    searchQuotes();
+  // Agregar evento 'keydown' al campo de búsqueda
+  searchInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      searchQuotes();
+    }
   });
+
+  // Función para buscar citas por personaje, frase o número de frases
+  async function searchQuotes() {
+    const searchTerm = searchInput.value.trim().toLowerCase();
+
+    try {
+      const response = await fetch(
+        `https://thesimpsonsquoteapi.glitch.me/quotes?count=100` // Asumiendo un máximo de 100 citas por búsqueda
+      );
+      const data = await response.json();
+
+      quoteContainer.innerHTML = ""; // Limpiar contenedor de citas
+
+      if (!searchTerm) {
+        // Si el término de búsqueda está vacío
+        for (let i = 0; i < charactersPerPage; i++) {
+          const { quote, character, image } = data[i];
+          const articleElement = createArticleElement(quote, character, image);
+          quoteContainer.appendChild(articleElement);
+        }
+      } else if (!isNaN(searchTerm)) {
+        // Si el término de búsqueda es un número
+        const characterQuotesMap = new Map();
+        data.forEach((quoteData) => {
+          const character = quoteData.character.toLowerCase();
+          if (!characterQuotesMap.has(character)) {
+            characterQuotesMap.set(character, []);
+          }
+          characterQuotesMap.get(character).push(quoteData);
+        });
+
+        characterQuotesMap.forEach((quotes, character) => {
+          if (quotes.length === parseInt(searchTerm)) {
+            quotes.forEach((quoteData) => {
+              const { quote, character, image } = quoteData;
+              const articleElement = createArticleElement(
+                quote,
+                character,
+                image
+              );
+              quoteContainer.appendChild(articleElement);
+            });
+          }
+        });
+      } else {
+        // Si el término de búsqueda no es un número y no está vacío
+        data.forEach((quoteData) => {
+          const { quote, character, image } = quoteData;
+          if (
+            character.toLowerCase().includes(searchTerm) ||
+            quote.toLowerCase().includes(searchTerm)
+          ) {
+            const articleElement = createArticleElement(
+              quote,
+              character,
+              image
+            );
+            quoteContainer.appendChild(articleElement);
+          }
+        });
+      }
+    } catch (error) {
+      console.error("Error al obtener las citas:", error);
+    }
+  }
 });
